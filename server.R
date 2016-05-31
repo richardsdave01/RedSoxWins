7d043097ffae3e7421bce16ce7e3189ddbcb6dfe
Dave Richards <richardsdave01@gmail.com>
2016-05-31 19:36:32 -0400

    App finally works, needs comments

--- server.R
+++ server.R
@@ -1,5 +1,3 @@
-
-
 library(shiny)
 library(Lahman)
 data("Teams")
@@ -7,34 +5,35 @@ library(dplyr)
 library(ggplot2)
 
 #  create RedSox dataset
-RedSox <-
-        Teams %>% filter(lgID == 'AL' &
-                                 teamID == 'BOS') %>% select(yearID, G, W, R, RA)
+RedSox <- Teams %>% filter(lgID == 'AL' & teamID == 'BOS') %>% 
+                    select(yearID, G, W, R, RA)
 
 
 shinyServer(function(input, output) {
-        
-        yearStats <-
-                filter(RedSox, RedSox$yearID >= input$yearRange[1] &
-                               RedSox$yearID <= input$yearRange[2])
-        ActWins <-
-                yearStats %>% select(yearID, W) %>% mutate(group = "Actual")
-        ExpWins <-
-                yearStats %>% mutate(W = ((R^2) / (R^2 + RA^2)) * G) %>%
-                select(yearID, W) %>% 
-                mutate(group = "Expected")
-        Wins <- rbind(ExpWins, ActWins)
         
         
         output$low <- renderText({ paste("First Year: ", input$yearRange[1]) })
         output$high <- renderText({ paste("Last Year: ", input$yearRange[2]) })
         
-  output$winsPlot <- renderPlot({
-
-          ggplot(Wins, aes(x=yearID, y=W, group=group, fill=group)) +
-                  geom_bar(stat = "identity", position = "dodge") +
-                  scale_fill_brewer(palette = "Set1") +
-                  labs(x = "Year", y = "Wins", fill = "")
-
-  })
+        output$winsPlot <- renderPlot({
+                
+                yearStats <-
+                        filter(RedSox, RedSox$yearID >= input$yearRange[1] &
+                                       RedSox$yearID <= input$yearRange[2])
+                yearStats$yearID <- as.factor(yearStats$yearID)
+                
+                ActWins <-
+                        yearStats %>% select(yearID, W) %>% mutate(group = "Actual")
+                ExpWins <-
+                        yearStats %>% mutate(W = ((R^2) / (R^2 + RA^2)) * G) %>%
+                        select(yearID, W) %>% 
+                        mutate(group = "Expected")
+                Wins <- rbind(ExpWins, ActWins)
+                
+                ggplot(Wins, aes(x=yearID, y=W, group=group, fill=group)) +
+                        geom_bar(stat = "identity", position = "dodge") +
+                        scale_fill_brewer(palette = "Set1") +
+                        labs(x = "Year", y = "Wins", fill = "")
+                
+        })
 })
