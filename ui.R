51d049255611ad0a2905a548eb959a49f1911123
Dave Richards <richardsdave01@gmail.com>
2016-06-01 21:03:06 -0400

    Added documentation to ui.R

--- ui.R
+++ ui.R
@@ -19,7 +19,16 @@ shinyUI(fluidPage(
                   min = 1901,
                   max = 2014,
                   value = c(1950, 1960),
-                  sep = "")
+                  sep = ""),
+      p("This application allows you to compare expected and actual wins for the
+        Boston Red Sox over a range of years between 1901 and 2014."),
+      p("Use the sliders to select the range of years that you want to analyze. 
+        The plot is most readable when the range is held to 20 or fewer years."),
+      p("The", span("red", style="color:red"), " bars show actual runs scored in each year."),
+      p("The", span("blue", style="color:blue"), " bars show expected runs scored
+         as calculated for each year using the formula:"),
+      withMathJax("$$ExpWins = \\frac{R^2}{R^2 + RA^2} \\cdot G$$"),
+      p("where R = runs scored, RA = runs allowed, and G = games played")
     ),
 
     # Show minimum & maximum years selected and
